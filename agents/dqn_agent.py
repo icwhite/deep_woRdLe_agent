86a6fcb9655b63c9b7a59e2f941f696e7785142d
seantsung <66893796+seantsung@users.noreply.github.com>
2022-11-29 17:47:52 -0800

    Added evaluation

--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -8,9 +8,10 @@ from utils.buffer import Buffer
 import utils.pytorch_utils as ptu
 from utils.logger import Logger
 
-class DQNAgent: 
 
-    def __init__(self, env, **params): 
+class DQNAgent:
+
+    def __init__(self, env, **params):
 
         # Grab attributes from environment
         self.env = env
@@ -19,36 +20,63 @@ class DQNAgent:
         self.params = params
 
         # Create Online and Target networks
-        self.online_net = MLPPolicy(obs_dim, ac_dim, params['n_layers'], params['size'], params['activation'], params['output_activation'])
-        self.target_net = MLPPolicy(obs_dim, ac_dim, params['n_layers'], params['size'], params['activation'], params['output_activation'])
+        self.online_net = MLPPolicy(obs_dim, ac_dim, params['n_layers'], params['size'], params['activation'],
+                                    params['output_activation'])
+        self.target_net = MLPPolicy(obs_dim, ac_dim, params['n_layers'], params['size'], params['activation'],
+                                    params['output_activation'])
 
         self.target_net.load_state_dict(self.online_net.state_dict())
-        self.optimizer = torch.optim.Adam(self.online_net.parameters(), lr = params['lr'])
+        self.optimizer = torch.optim.Adam(self.online_net.parameters(), lr=params['lr'])
 
         self.logger = Logger(self.params["logdir"])
 
-
         # Initialize replay buffers and fill
-        self.buffer = Buffer(params['replay_buffer_size'], params['min_replay_size'], params['reward_buffer_size'], env, fill = True)
+        self.buffer = Buffer(params['replay_buffer_size'], params['min_replay_size'], params['reward_buffer_size'], env,
+                             fill=True)
         self.replay_buffer = self.buffer.replay_buffer
         self.reward_buffer = self.buffer.reward_buffer
 
-
-    def train_agent(self): 
-        
+    def evaluate_agent(self):
+        num_episodes = 0
+        eval_rewards = []
         obs = self.env.reset()
         episode_reward = 0
-        for iter in range(self.params['n_iter']): 
+        while num_episodes < self.params['num_eval_episodes']:
+            action = self.online_net.get_action(obs)
+            # Take the action and store transition
+            next_obs, rew, done, info = self.env.step(action)
+            obs = next_obs
+            episode_reward += rew
 
+            # Reset the environment if necessary
+            if done:
+                obs = self.env.reset()
+                eval_rewards.append(episode_reward)
+                episode_reward = 0
+                num_episodes += 1
+
+        average_eval_reward = np.mean(eval_rewards)
+        std_eval_reward = np.std(eval_rewards)
+
+        return average_eval_reward, std_eval_reward
+
+    def train_agent(self):
+
+        obs = self.env.reset()
+        num_episodes = 0
+        episode_reward = 0
+        for iter in range(self.params['n_iter']):
+            new_episode = False
             # Compute epsilon 
-            epsilon = np.interp(iter, [0, self.params['epsilon_decay']], [self.params['epsilon_start'], self.params['epsilon_end']])
+            epsilon = np.interp(iter, [0, self.params['epsilon_decay']],
+                                [self.params['epsilon_start'], self.params['epsilon_end']])
             random_val = random.random()
 
             # Take random action with probability epsilon
             if random_val <= epsilon:
                 action = self.env.action_space.sample()
 
-            else:  
+            else:
                 # Otherwise use the online network to take the action
                 action = self.online_net.get_action(obs)
 
@@ -61,10 +89,12 @@ class DQNAgent:
             episode_reward += rew
 
             # Reset the environment if necessary
-            if done: 
+            if done:
                 obs = self.env.reset()
                 self.reward_buffer.append(episode_reward)
-                episode_reward = 0 
+                episode_reward = 0
+                num_episodes += 1
+                new_episode = True
 
             # Sample transitions from the replay buffer
             transitions = random.sample(self.replay_buffer, self.params['batch_size'])
@@ -76,20 +106,19 @@ class DQNAgent:
             dones_t = ptu.from_numpy(np.array([t[3] for t in transitions]))
             next_obses_t = ptu.from_numpy(np.array([t[4] for t in transitions], dtype=np.int64))
 
-
             # Fix datatypes
             obses_t = obses_t.to(torch.int64)
             next_obses_t = next_obses_t.to(torch.int64)
             actions_t = actions_t.to(torch.int64)
-            
+
             # Compute targets
             next_q = self.target_net(next_obses_t)
-            target_q = (next_q.max(dim = 1, keepdim = True)[0]).squeeze(-1)
-            targets = rewards_t + self.params['gamma'] * (1 - dones_t) * target_q # 32, 32
+            target_q = (next_q.max(dim=1, keepdim=True)[0]).squeeze(-1)
+            targets = rewards_t + self.params['gamma'] * (1 - dones_t) * target_q  # 32, 32
 
             # Compute loss 
             current_q = self.online_net(obses_t)
-            action_q_values = torch.gather(input = current_q, dim = 1, index = actions_t).squeeze(-1) # 32,1
+            action_q_values = torch.gather(input=current_q, dim=1, index=actions_t).squeeze(-1)  # 32,1
             loss = nn.functional.smooth_l1_loss(action_q_values, targets)
 
             # Step Optimizer 
@@ -98,22 +127,28 @@ class DQNAgent:
             self.optimizer.step()
 
             # Update target network 
-            if iter % self.params['target_update_freq'] == 0: 
+            if iter % self.params['target_update_freq'] == 0:
                 self.target_net.load_state_dict(self.online_net.state_dict())
 
             # Logging
-            if iter % self.params['log_period'] == 0:
+            # log_period now refers to number of episodes
+            if new_episode and num_episodes % self.params['log_period'] == 0:
+                average_eval_reward, std_eval_reward = self.evaluate_agent()
                 logs = {
                     "Average Train Reward": np.mean(self.reward_buffer),
+                    "Std Train Reward": np.std(self.reward_buffer),
+                    "Average Eval Reward": average_eval_reward,
+                    "Std Eval Reward": std_eval_reward,
                     "Training Loss": loss
                 }
-                self.do_logging(logs, iter)
+                self.do_logging(logs, num_episodes, iter)
                 # print(f'\nIteration {iter}')
                 # print(f'Avgerage Train Reward: {np.mean(self.reward_buffer)}')
 
                 # Compute some evaluation reward by running a new 100 games and computing the average reward
 
-    def do_logging(self, logs, step):
+    def do_logging(self, logs, num_episodes, step):
+        print(f"Episode: {num_episodes}")
         print(f"Iteration: {step}")
         for key, value in logs.items():
             print('{} : {}'.format(key, value))
