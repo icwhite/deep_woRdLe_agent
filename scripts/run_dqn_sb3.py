b54234fefd0c3acdeeb39d9ea3f4933ff672de59
seantsung <66893796+seantsung@users.noreply.github.com>
2022-12-01 13:56:59 -0800

    Update run_dqn_sb3.py

--- scripts/run_dqn_sb3.py
+++ scripts/run_dqn_sb3.py
@@ -10,7 +10,7 @@ env = Wordle(n_boards = 1,
 
 data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data')
 # data_path = "Users/isado/cs285/cs285_final_project/data/"
-logdir = "stable_baseline_dqn_no_fixed_answer" + 'learning_rate_0.3' + time.strftime("%d-%m-%Y_%H-%M-%S")
+logdir = "stable_baseline_dqn_no_fixed_answer" + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
 logging = os.path.join(data_path, logdir)
 
 # Run DQN: Link to docs (https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html)
