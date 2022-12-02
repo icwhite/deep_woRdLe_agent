3500a833ab8a0d8aa1a86c686d7335b99a858a60
Isadora White <isadora.c.white@gmail.com>
2022-12-02 09:13:48 -0800

    running experiments

--- ablation_valid_words_ppo.bat
+++ ablation_valid_words_ppo.bat
@@ -1,4 +1,4 @@
 
 python scripts/run_ppo_sb3.py --exp_name 100_12 --subset_valid_words 100 --subset_answers 12
 python scripts/run_ppo_sb3.py --exp_name 100_50 --subset_valid_words 100 --subset_answers 50
-python scripts/run_ppo_sb3.py --exp_name 1000_100 --subset_answers 1000 --subset_answers 100
+python scripts/run_ppo_sb3.py --exp_name 100_100 --subset_answers 100
