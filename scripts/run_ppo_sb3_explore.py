b9c5cc2ae8e828373ea7779edc5906d0e12afc6c
Isadora White <isadora.c.white@gmail.com>
2022-12-04 15:21:58 -0800

    implementing basic exploration models and minor refactor for wordle environment

--- scripts/run_ppo_sb3_explore.py
+++ scripts/run_ppo_sb3_explore.py
@@ -1,5 +1,7 @@
 import stable_baselines3 as sb3
 from utils.environments.wordle import Wordle
+from utils.exploration.count_explore_model import CountExploreModel
+
 import os
 import time
 import argparse
@@ -18,7 +20,8 @@ env = Wordle(n_boards=1,
              n_guesses=6,
              subset_valid_words=params["subset_valid_words"],
              subset_answers=params["subset_answers"],
-             keep_answers_on_reset=False)
+             keep_answers_on_reset=False,
+             exploration_model= CountExploreModel())
 
 data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data')
 # data_path = "Users/isado/cs285/cs285_final_project/data/"
@@ -27,14 +30,14 @@ logging = os.path.join(data_path, logdir)
 
 # Run DQN: Link to docs (https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html)
 agent = sb3.PPO(policy = 'MlpPolicy',
-                env = env, 
-                learning_rate = 0.0003, 
-                n_steps = 2048, 
-                batch_size = 64, 
-                n_epochs = 10, 
-                gamma = 0.99, 
-                gae_lambda = 0.95, 
-                clip_range = 0.2, 
+                env = env,
+                learning_rate = 0.0003,
+                n_steps = 2048,
+                batch_size = 64,
+                n_epochs = 10,
+                gamma = 0.99,
+                gae_lambda = 0.95,
+                clip_range = 0.2,
                 verbose = 1,
                 tensorboard_log=logging)
 agent.learn(total_timesteps = 1_000_000, log_interval = 4) # remember total times steps is number of guesses NOT number of games
