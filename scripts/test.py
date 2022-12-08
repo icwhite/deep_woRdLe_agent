d860984b43fdb29b12eb9c7053a118206114eb00
Bennett Cohen <bennettcohen@Bennetts-MacBook-Air.local>
2022-12-07 20:22:53 -0800

    added wordle simple

---
+++ scripts/test.py
@@ -0,0 +1,26 @@
+import stable_baselines3 as sb3
+from utils.environments.wordle import Wordle
+import os
+import time
+import argparse
+
+# Get wordle words
+wordle_words = open("scripts/wordle_words.txt", "r").read().split(",")
+wordle_words = [word.replace('\n', '') for word in wordle_words]
+
+# Create Environment
+env = Wordle(n_boards=1,
+             n_letters=5,
+             n_guesses=6,
+             answers = ['idiot'], 
+            keep_answers_on_reset=True)
+
+model = sb3.PPO.load("saved_agents/ppo_lr_0.003")
+# Enjoy trained agent
+obs = env.reset()
+print(env.answers)
+done = False
+while not done:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    print(env.valid_words[action])
